ba7bada9fdcef3d062f293dfcce5af12a6c2b60a
Michael White <michaelwhite404@gmail.com>
2022-11-09 13:25:37 -0500

    Login Page Edits

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,8 +1,9 @@
-import { InputGroup, Label } from "@blueprintjs/core";
+import { Label } from "@blueprintjs/core";
 import { useState } from "react";
 import Div100vh from "react-div-100vh";
 import CornerstoneLogo from "../../components/CornerstoneLogo";
 import Login from "../../components/Login";
+import LabeledInput2 from "../../components/LabeledInput2";
 import PrimaryButton from "../../components/PrimaryButton/PrimaryButton";
 import TextOverLine from "../../components/TextOverLine";
 import { useAuth, useDocTitle, useToasterContext } from "../../hooks";
@@ -36,7 +37,7 @@ export default function Home() {
           </div>
           <div>
             <Label>
-              <span style={{ fontWeight: 500 }}>Sign in with Cornestone Gmail Account</span>
+              <span style={{ fontWeight: 500 }}>Sign in with Cornerstone Gmail Account</span>
             </Label>
             <Login />
           </div>
@@ -45,30 +46,25 @@ export default function Home() {
           </span>
           <div className="sign-in-form">
             <form onSubmit={handleSubmit}>
-              <div style={{ padding: "5px 0" }}>
-                <Label>
-                  <span style={{ fontWeight: 500 }}>Email Address</span>
-                  <InputGroup
-                    fill
-                    value={credentials.email}
-                    onChange={handleChange}
-                    name="email"
-                    required
-                  />
-                </Label>
+              <div className="mb-4 pb-1.5">
+                <LabeledInput2
+                  label="Email Address"
+                  value={credentials.email}
+                  onChange={handleChange}
+                  name="email"
+                  type="email"
+                  required
+                />
               </div>
-              <div style={{ padding: "5px 0" }}>
-                <Label>
-                  <span style={{ fontWeight: 500 }}>Password</span>
-                  <InputGroup
-                    fill
-                    value={credentials.password}
-                    onChange={handleChange}
-                    name="password"
-                    type="password"
-                    required
-                  />
-                </Label>
+              <div className="mb-4 pb-1.5">
+                <LabeledInput2
+                  label="Password"
+                  value={credentials.password}
+                  onChange={handleChange}
+                  name="password"
+                  type="password"
+                  required
+                />
               </div>
               <PrimaryButton text="Sign In" type="submit" fill />
             </form>
