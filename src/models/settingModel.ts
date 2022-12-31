69284a9a2312d378b929e2c5f61ab91ac6f5df35
Michael White <michaelwhite404@gmail.com>
2022-12-30 22:15:55 -0500

    Add setting & userSetting model

---
+++ src/models/settingModel.ts
@@ -0,0 +1,35 @@
+import { model, Schema } from "mongoose";
+
+const settingSchema = new Schema({
+  name: { type: String, required: true, unique: true },
+  category: { type: String, required: true },
+  description: { type: String, required: true },
+  settingName: { type: String, required: true },
+  settingType: { type: String, required: true },
+  fieldDescriptions: [
+    {
+      field: { type: String, required: true },
+      fieldDescription: { type: String, default: "N/A" },
+      name: { type: String, required: true },
+      defaultValue: { type: String, required: false },
+      knownValueDescriptions: [{ value: String, description: String }],
+      type: {
+        type: String,
+        required: true,
+        enum: [
+          "TYPE_ENUM",
+          "TYPE_BOOL",
+          "TYPE_STRING",
+          "TYPE_DOUBLE",
+          "TYPE_FLOAT",
+          "TYPE_INT32",
+          "TYPE_INT64",
+        ],
+      },
+    },
+  ],
+});
+
+const Setting = model("Setting", settingSchema);
+
+export default Setting;
---
+++ src/models/userSettingModel.ts
@@ -0,0 +1,21 @@
+import { model, Schema } from "mongoose";
+
+const userSettingSchema = new Schema({
+  setting: {
+    type: Schema.Types.ObjectId,
+    ref: "Setting",
+    required: [true, "Each user setting must reference a setting"],
+    immutable: true,
+  },
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "Employee",
+    required: [true, "Each user setting must reference a user"],
+    immutable: true,
+  },
+  value: Schema.Types.Mixed,
+});
+
+const UserSetting = model("UserSetting", userSettingSchema);
+
+export default UserSetting;
