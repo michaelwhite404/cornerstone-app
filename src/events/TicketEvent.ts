6cf7d481cbdda400d33acdb9fd84db22be69c012
Michael White <michaelwhite404@gmail.com>
2023-01-11 21:18:06 -0500

    Refactor assign message objects

--- src/events/TicketEvent.ts
+++ src/events/TicketEvent.ts
@@ -7,86 +7,32 @@ import {
 import { Ticket } from "@models";
 import { chat } from "@utils";
 import capitalize from "capitalize";
+import { chat_v1 } from "googleapis";
 
 const URL =
   process.env.NODE_ENV! === "development"
     ? "http://localhost:3000"
     : "https://app.cornerstone-schools.org";
 
+interface AssignMessageData {
+  ticket: TicketDocument;
+  parent: string;
+  text: string;
+  headerTitle: string;
+}
 class TicketEvent {
-  async submit(ticket: TicketDocument) {
-    ticket = await Ticket.populate(ticket, { path: "assignedTo submittedBy" });
-    const assignedTo = ticket.assignedTo as EmployeeDocument[];
-    const spaces = assignedTo.filter((e) => e.space).map((e) => e.space) as string[];
-    spaces.forEach(async (space) => {
-      await chat.spaces.messages.create({
-        parent: space,
-        requestBody: {
-          text: `${ticket.submittedBy.fullName} has submitted a ticket`,
-          cards: [
-            {
-              header: {
-                title: "New Ticket",
-                subtitle: `#${ticket.ticketId}`,
-                imageUrl: "https://i.ibb.co/Ypsrycx/Ticket-Blue.png",
-              },
-              sections: [
-                {
-                  widgets: [
-                    { keyValue: { topLabel: "Title", content: ticket.title } },
-                    { keyValue: { topLabel: "Description", content: ticket.description } },
-                    {
-                      keyValue: {
-                        topLabel: "Priority",
-                        content: capitalize(ticket.priority.toLowerCase()),
-                      },
-                    },
-                  ],
-                },
-                {
-                  widgets: [
-                    {
-                      buttons: [
-                        {
-                          textButton: {
-                            text: "OPEN IN APP",
-                            onClick: {
-                              openLink: {
-                                url: `${URL}/tickets/${ticket.ticketId}`,
-                              },
-                            },
-                          },
-                        },
-                      ],
-                    },
-                  ],
-                },
-              ],
-            },
-          ],
-        },
-      });
-    });
-  }
-
-  async assign(update: TicketAssignUpdateDocument) {
-    const ticket = await Ticket.findById(update.ticket).populate({
-      path: "assignedTo submittedBy",
-    });
-    if (!ticket) return;
-
-    const user = (ticket.assignedTo as EmployeeDocument[]).find(
-      (user) => user._id.toString() === update.assign.toString()
-    );
-    if (!user || !user.space) return;
-    await chat.spaces.messages.create({
-      parent: user.space,
+  private createAssignMessage(
+    data: AssignMessageData
+  ): chat_v1.Params$Resource$Spaces$Messages$Create {
+    const { ticket, parent, text, headerTitle } = data;
+    return {
+      parent,
       requestBody: {
-        text: `You have been assigned to a ticket`,
+        text,
         cards: [
           {
             header: {
-              title: "Assigned Ticket",
+              title: headerTitle,
               subtitle: `#${ticket.ticketId}`,
               imageUrl: "https://i.ibb.co/Ypsrycx/Ticket-Blue.png",
             },
@@ -125,7 +71,41 @@ class TicketEvent {
           },
         ],
       },
+    };
+  }
+
+  async submit(ticket: TicketDocument) {
+    ticket = await Ticket.populate(ticket, { path: "assignedTo submittedBy" });
+    const assignedTo = ticket.assignedTo as EmployeeDocument[];
+    const spaces = assignedTo.filter((e) => e.space).map((e) => e.space) as string[];
+    spaces.forEach(async (space) => {
+      const params = this.createAssignMessage({
+        ticket,
+        parent: space,
+        text: `${ticket.submittedBy.fullName} has submitted a ticket`,
+        headerTitle: "New Ticket",
+      });
+      await chat.spaces.messages.create(params);
     });
+  }
+
+  async assign(update: TicketAssignUpdateDocument) {
+    const ticket = await Ticket.findById(update.ticket).populate({
+      path: "assignedTo submittedBy",
+    });
+    if (!ticket) return;
+
+    const user = (ticket.assignedTo as EmployeeDocument[]).find(
+      (user) => user._id.toString() === update.assign.toString()
+    );
+    if (!user || !user.space) return;
+    const params = this.createAssignMessage({
+      ticket,
+      parent: user.space,
+      text: "You have been assigned to a ticket",
+      headerTitle: "Assigned Ticket",
+    });
+    await chat.spaces.messages.create(params);
   }
 
   async comment(update: TicketCommentUpdateDocument) {
